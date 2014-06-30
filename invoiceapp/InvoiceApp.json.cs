f22474c745558a18cc546bf2a13f0e9e4597ffa4
Marcin Warpechowski <warpech@gmail.com>
2014-06-30 12:57:21 +0200

    revert null check (requires Starcounter 2.0.2177.3)

--- invoiceapp/InvoiceApp.json.cs
+++ invoiceapp/InvoiceApp.json.cs
@@ -115,14 +115,7 @@ partial class InvoiceApp : Json {
 public class Invoice {
     public int InvoiceNo;
     public string Name;
-    public decimal Total { get { 
-        var sum = Db.SQL<decimal?>("SELECT sum(o.Total) FROM InvoiceRow o WHERE o.Invoice=?", this).First;
-        if (sum == null)
-        {
-            return 0;
-        }
-        return (decimal)sum; 
-    } }
+    public decimal Total { get { return Db.SQL<decimal>("SELECT sum(o.Total) FROM InvoiceRow o WHERE o.Invoice=?", this).First; } }
     public IEnumerable Items { get { return Db.SQL<InvoiceRow>("SELECT o FROM InvoiceRow o WHERE o.Invoice=?", this); } }
 }
 
