a9a036399cafb74b8798039afa8036d1e9085781
Marcin Warpechowski <warpech@gmail.com>
2015-05-06 12:49:15 +0200

    move current code to InvoiceDemo2

--- README.md
+++ README.md
@@ -2,10 +2,6 @@
 
 This is a series of tutorials about building your first in-memory computing app:
 
-- [InvoiceDemo1](https://github.com/Starcounter/InvoiceDemo1) :construction: - show a single invoice with rows, computed totals and buttons: Add Row, Save and Cancel
-- [InvoiceDemo2](https://github.com/Starcounter/InvoiceDemo2) - add a list of invoices, Delete button, partial templates and URL morphing
-- [InvoiceDemo3](https://github.com/Starcounter/InvoiceDemo3) :construction: - add mapping to integrate your app with another apps running in the database
-
-# InvoiceDemo2
-
-This is the first complete demo, that has parts of InvoiceDemo1 and InvoiceDemo3. See the wiki for the tutorial.
+- **InvoiceDemo1** - show a single invoice with rows, computed totals and buttons: Add Row, Save and Cancel
+- **InvoiceDemo2** - add a list of invoices, Delete button, partial templates and URL morphing
+- **InvoiceDemo3** :construction: - add mapping to integrate your app with another apps running in the database
--- src/InvoiceDemo2/Properties/AssemblyInfo.cs
+++ src/InvoiceDemo2/Properties/AssemblyInfo.cs
@@ -1,15 +1,16 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Starcounter.Internal;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InvoiceApp")]
+[assembly: AssemblyTitle("InvoiceDemo")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InvoiceApp")]
+[assembly: AssemblyProduct("InvoiceDemo")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -34,3 +35,8 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Assures the current assembly has a reference to the Starcounter
+// assembly. A reference to Starcounter is currently required for
+// Starcounter to detect that an assembly should be hosted.
+[assembly: StarcounterAssembly()]
