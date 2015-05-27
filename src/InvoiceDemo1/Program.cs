1742999c496bf186ae1795c095e7166db3c41b07
Marcin Warpechowski <warpech@gmail.com>
2015-05-27 12:42:22 +0200

    reuse existing session (otherwise the first patch replaces the viewmodel)

--- src/InvoiceDemo1/Program.cs
+++ src/InvoiceDemo1/Program.cs
@@ -4,13 +4,18 @@ using Starcounter;
 class Program {
     static void Main() {
         Handle.GET("/invoicedemo", () => {
-            InvoicePage page = Db.Scope<InvoicePage>(() => {
-                return new InvoicePage() {
-                    Html = "/InvoicePage.html",
-                    Data = new Invoice()
-                };
-            });
-            page.Session = new Session(SessionOptions.PatchVersioning);
+            InvoicePage page;
+            if (Session.Current != null) {
+                page = (InvoicePage)Session.Current.Data;
+            } else {
+                page = Db.Scope<InvoicePage>(() => {
+                    return new InvoicePage() {
+                        Html = "/InvoicePage.html",
+                        Data = new Invoice()
+                    };
+                });
+                page.Session = new Session(SessionOptions.PatchVersioning);
+            }
             return page;
         });
 
