3a0f7214dfb293718c57df8781fa719496c3f3c9
miyconst <const.miyang@gmail.com>
2015-06-22 20:51:42 +0300

    Able to look up Organizations and Products
    https://github.com/Starcounter/InvoiceDemo/issues/21

--- src/InvoiceDemo3/InvoicePage.json.cs
+++ src/InvoiceDemo3/InvoicePage.json.cs
@@ -43,6 +43,18 @@ partial class InvoicePage : Page, IBound<Invoice> {
         RedirectUrl = "/invoicedemo"; //redirect to the home URL		
     }
 
+    void Handle(Input.Name action) {
+        this.FoundSomebodies.Clear();
+
+        if (!string.IsNullOrEmpty(action.Value)) {
+            this.FoundSomebodies = Db.SQL("SELECT s FROM Simplified.Ring1.Somebody s WHERE s.Name LIKE ? ORDER BY s.Name FETCH ?", "%" + action.Value + "%", 8);
+        }
+    }
+
+    void Handle(Input.ClearFoundSomebodies action) {
+        this.FoundSomebodies.Clear();
+    }
+
     protected void OnSaved() {
         if (this.Saved != null) {
             this.Saved(this, EventArgs.Empty);
@@ -52,6 +64,49 @@ partial class InvoicePage : Page, IBound<Invoice> {
     protected void OnDeleted() {
         if (this.Deleted != null) {
             this.Deleted(this, EventArgs.Empty);
+        }
+    }
+
+    [InvoicePage_json.FoundSomebodies]
+    public partial class InvoicePageFoundSomebodies : Json, IBound<Simplified.Ring1.Somebody> {
+        void Handle(Input.Choose action) {
+            this.ParentPage.Name = this.Name;
+            this.ParentPage.FoundSomebodies.Clear();
+        }
+
+        InvoicePage ParentPage {
+            get {
+                return this.Parent.Parent as InvoicePage;
+            }
+        }
+    }
+
+    [InvoicePage_json.Items]
+    public partial class InvoicePageItems : Json, IBound<InvoiceRow> {
+        void Handle(Input.Description action) {
+            this.FoundProducts.Clear();
+
+            if (!string.IsNullOrEmpty(action.Value)) {
+                this.FoundProducts = Db.SQL("SELECT p FROM Simplified.Ring3.Product p WHERE p.Name LIKE ? ORDER BY p.Name FETCH ?", "%" + action.Value + "%", 8);
+            }
+        }
+
+        void Handle(Input.ClearFoundProducts action) {
+            this.FoundProducts.Clear();
+        }
+    }
+
+    [InvoicePage_json.Items.FoundProducts]
+    public partial class InvoicePageItemsFoundProducts : Json, IBound<Simplified.Ring3.Product> {
+        void Handle(Input.Choose action) {
+            this.ParentPage.Description = this.Name;
+            this.ParentPage.FoundProducts.Clear();
+        }
+
+        InvoicePageItems ParentPage {
+            get {
+                return this.Parent.Parent as InvoicePageItems;
+            }
         }
     }
 }
