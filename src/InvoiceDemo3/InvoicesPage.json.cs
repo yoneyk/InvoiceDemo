8f027f62a4d83aeb07d96122f454365d20583685
Konstantin <const.miyang@gmail.com>
2015-10-26 10:38:06 +0100

    Update InvoiceDemo3 to match InvoiceDemo2
    https://github.com/Starcounter/InvoiceDemo/issues/34

--- src/InvoiceDemo3/InvoicesPage.json.cs
+++ src/InvoiceDemo3/InvoicesPage.json.cs
@@ -4,4 +4,12 @@ partial class InvoicesPage : Page {
     public void RefreshData() {
         Invoices = Db.SQL("SELECT i FROM Invoice i");
     }
+
+    [InvoicesPage_json.Invoices]
+    partial class InvoicesItemPage {
+        protected override void OnData() {
+            base.OnData();
+            this.Url = string.Format("/invoicedemo/invoices/{0}", this.InvoiceNo);
+        }
+    }
 }
